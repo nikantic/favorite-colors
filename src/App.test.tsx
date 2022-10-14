c186cc7d7ffba65de250bb5b80b041a60d7f3f2f
Nikola Antic <nikantic@outlook.com>
2022-10-15 01:03:19 +0200

    Add simple test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import List from "./components/List";
+import store from "./store/store";
+
+test("renders header", () => {
+	render(
+		<Provider store={store}>
+			<BrowserRouter>
+				<List />
+			</BrowserRouter>
+		</Provider>
+	);
+	const header = screen.getByText(/Favorite Colors/i);
+	expect(header).toBeInTheDocument();
 });
